57d600d49507d7e1550844ad8278f61c5bf46a12
lovesaikrishna <44071983+lovesaikrishna@users.noreply.github.com>
2021-03-22 11:52:15 -0400

    CronJob
    CronJob

--- health.sh
+++ health.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /usr/bin/echo "   " > /var/tmp/health.txt
-/usr/bin/echo "Bingo! CronJob Deployed Successfully " >> /var/tmp/health.txt
+/usr/bin/echo "Bingo!! CronJob Deployed Successfully" >> /var/tmp/health.txt
 /usr/bin/echo "   " >> /var/tmp/health.txt
 /usr/bin/hostname -f  >> /var/tmp/health.txt
 /usr/bin/echo "   " >> /var/tmp/health.txt
